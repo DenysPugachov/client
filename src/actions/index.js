1938efcb48c587c6b1a04093fd57be5dd6658f26
Denys <pugachovdenys@gmail.com>
2021-02-28 23:24:05 +0200

    add programmatic navigation after creating a stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import streams from "../apis/streams"
+import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -25,33 +26,36 @@ export const signOut = () => {
 
 
 export const createStream = formValues => async (dispatch, getState) => {
+  //get userId form state.auth
   const { userId } = getState().auth;
+
   const response = await streams.post("./streams", { ...formValues, userId });
 
-  return dispatch({ type: CREATE_STREAM, payload: response.data });
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 
   //do some programmatic navigation to get user back to the root route => "https://localhost:3000/"
+  history.push("/");
 }
 
 export const fetchStream = id => async dispatch => {
   const response = await streams.get(`./streams/${id}`);
-  return dispatch({ type: FETCH_STREAM, payload: response.data.id });
+  dispatch({ type: FETCH_STREAM, payload: response.data.id });
 }
 
 
 export const fetchStreams = () => async dispatch => {
   const response = await streams.get("/streams");
-  return dispatch({ type: FETCH_STREAMS, payload: response.data })
+  dispatch({ type: FETCH_STREAMS, payload: response.data })
 }
 
 
 export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.put(`/streams/${id}`, formValues)
-  return dispatch({ type: EDIT_STREAM, payload: response.data })
+  dispatch({ type: EDIT_STREAM, payload: response.data })
 }
 
 
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
-  return dispatch({ type: DELETE_STREAM, payload: id })
+  dispatch({ type: DELETE_STREAM, payload: id })
 }
