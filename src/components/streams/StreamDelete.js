27ffc751dabc4bf14dfdf729a4553a71f36efefb
Denys <pugachovdenys@gmail.com>
2021-03-11 16:31:29 +0200

    add reusable modal compontent 370

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Modal from "../Modal"
+
 
 const StreamDelete = () => {
+
+  const actions = (
+    <>
+      <button className="ui button ">Cancel</button>
+      <button className="ui button negative">Delete</button>
+    </>
+  )
+
+
   return (
-    <div>StreamDelete</div>
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={ actions }
+      />
+    </div>
   )
 }
 
